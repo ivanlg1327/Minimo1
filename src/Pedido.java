80111ed29d833ab48ffe534532c3f5aa99ac37f2
Ivan <ivanl.g.27@hotmail.com>
2019-11-05 17:39:04 +0100

    Initial structure

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,2 +1,25 @@
+import java.util.List;
 public class Pedido {
+    List <Product> productList;
+    int cost=0;
+    String name;
+
+    public Pedido(List<Product> productList, String name) {
+        this.productList = productList;
+        this.name=name;
+        this.cost = this.count(productList);
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int count(List <Product> productList)
+    {
+        return 1;
+    }
 }
