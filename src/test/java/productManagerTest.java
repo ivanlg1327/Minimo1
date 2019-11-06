e4cec3254a19d573db6bd81bc6aa661cfbde9ba4
Ivan <ivanl.g.27@hotmail.com>
2019-11-06 10:03:41 +0100

    Desarrollo interficie

---
+++ src/test/java/productManagerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+import java.util.List;
+
+public class productManagerTest {
+    ProductManagerImp test1=null;
+    @Before
+    public void setUp(){
+        List<Product> productList;
+
+    }
+    @Test
+    public void testing(){
+        test1.listPrices();
+    }
+
+}
